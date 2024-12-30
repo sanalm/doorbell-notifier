b49597cf547ebdd49b22293c4f269babcf130e1c
Sanal Madhavan <sanal.madhavan@gmail.com>
2024-12-30 19:58:08 +0000

    reinstate ota updater

--- main/main.py
+++ main/main.py
@@ -33,10 +33,10 @@ def start():
     r.wait_bell_press()
 
 def boot():
-    # if 'next' in os.listdir():
-    #     download_and_install_update_if_available()
-    # else:
-    #     check_for_update_to_install_during_next_reboot()
+    if 'next' in os.listdir():
+        download_and_install_update_if_available()
+    else:
+        check_for_update_to_install_during_next_reboot()
     start()
 
 # ------------------------------------------------------------------------------------------
